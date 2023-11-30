4fa007c5a68a982e6cbd054b48b9b0a0c425be4b
prudnikov_ar <wordmoney787@gmail.com>
2023-11-30 23:07:52 +0700

    complete

---
+++ remot.py
@@ -0,0 +1,53 @@
+"""
+Вариант 9
+"""
+
+
+def bold(s: str) -> str:
+    pair = s.count("**")
+    if pair == 0:
+        return s
+    for _ in range(pair//2):
+        s = s.replace("**", "<strong>", 1).replace("**", "</strong>", 1)
+    return s
+
+
+def itallic(s: str) -> str:
+    once = s.count("*")
+    if once == 0:
+        return s
+    for _ in range(once//2):
+        s = s.replace("*", "<em>", 1).replace("*", "</em>", 1)
+    return s
+
+
+while True:
+
+    print("Меню")
+    print()
+    print("Введите 1 если хотите отформатировать строку курсивом")
+    print("Введите 2 если хотите отформатировать строку полужирным")
+    print("Введите 0 если хотите завершить программу")
+    print()
+
+    ans = input()
+    if ans == "1":
+        print("Введите строку")
+        s = input()
+        print("Ваша строка в отформатированнм виде:")
+        print()
+        print(itallic(s))
+        print()
+    elif ans == "2":
+        print("Введите строку")
+        s = input()
+        print("Ваша строка в отформатированнм виде:")
+        print()
+        print(bold(s))
+        print()
+    elif ans == "0":
+        print("Программа завершена")
+        break
+    else:
+        print("Такой программы не существует")
+        print()
